61c84f88dc89026b5e71df131bc1dfb6dae11314
Vincent Tam <sere@live.hk>
2018-11-14 13:18:14 +0100

    Adding support for reply

---
+++ static/js/staticman-reply.js
@@ -0,0 +1,5 @@
+// Added function to change value onclick
+// from: https://networkhobo.com/2017/12/30/hugo-staticman-nested-replies-and-e-mail-notifications/
+function changeValue(elementName, newValue){
+  document.getElementsByName(elementName)[0].value=newValue;
+};
