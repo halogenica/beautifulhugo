d969cc3f8c8cc18cba81addb24434c793d373afe
Vincent Tam <sere@live.hk>
2018-11-19 08:39:21 +0100

    Basic support for comment previews

--- static/js/staticman-preview.js
+++ static/js/staticman-preview.js
@@ -1,22 +1,43 @@
 $( document ).ready(function() {
+  var myKaTeXOptions = {
+    // LaTeX like math delimiters
+    delimiters: [
+    {left: "$$", right: "$$", display: true},
+    {left: "\\[", right: "\\]", display: true},
+    {left: "$", right: "$", display: false},
+    {left: "\\(", right: "\\)", display: false}
+    ],
+    throwOnError: false
+  };
+  renderMathInElement(document.body, myKaTeXOptions);
+
+  // Showdown extension has to preceed object definition
+  showdown.extension('unquote', function() {
+    return [
+    // repair math formula incorrectly parsed by Showdown
+    // from: https://blag.nullteilerfrei.de/2017/06/23/wordpress-katex-and-the-showdown/
+    { type: 'lang', regex:  /&gt;/g, replace: '>' },
+    { type: 'lang', regex:  /&lt;/g, replace: '<' },
+    { type: 'lang', regex: /&amp;/g, replace: '&' },
+    ]
+  });
+  // Showdown converts Markdown to HTML
+  var converter = new showdown.Converter({extensions:['unquote']});
+  converter.setFlavor('github');
+
   // Detect textarea event
+  var oldVal = "";
   $('textarea[name="fields[comment]"]').on("change keyup paste", function() {
-    var oldVal = "";
     var currentVal = $(this).val();
-    if(currentVal == oldVal) {
+    if(currentVal === oldVal) {
       return; // check to prevent multiple simultaneous triggers
     }
-
     oldVal = currentVal;
-    //action to be performed on textarea changed
-    renderMD($(this), $("#comment-preview"));
-    renderMathInElement(comment-preview);
-  });
 
-  // Showdown
-  var converter = new showdown.Converter({tables: true});
-  function renderMD(inElem, outElem){
-    rawHTML = converter.makeHtml(inElem.val());
-    outElem.html(rawHTML);
-  }
+    // first parse math, then parse Markdown
+    $('#commentPreview').text(oldVal);
+    renderMathInElement(commentPreview, myKaTeXOptions);
+    var rawHTML = converter.makeHtml($('#commentPreview').html());
+    $('#commentPreview').html(rawHTML);
+  });
 });
