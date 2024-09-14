86a4b1d87cc8932b4527cc58dfbc1ef85e84e483
陳俊安 (Chen Chun-An) <androchentw@gmail.com>
2024-09-14 21:53:40 +0800

    feat ui: back to top

--- static/js/main.js
+++ static/js/main.js
@@ -100,6 +100,22 @@ var main = {
         }
       });
     }
+
+    // feat ui: back to top
+    const $backToTop = $('#back-to-top');
+    if ($toc.length) {
+      $(window).scroll(function() {
+        if ($(window).scrollTop() > 100) {
+          $backToTop.fadeIn(1000);
+        } else {
+          $backToTop.fadeOut(1000);
+        }
+      });
+    }
+
+    $backToTop.click(function() {
+      $('body,html').animate({scrollTop: 0});
+    });
   },
 
   initImgs : function() {
