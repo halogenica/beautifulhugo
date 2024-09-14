3cc4ea0efe36fd47c6bb1dbf96862b0407eb1cdc
陳俊安 (Chen Chun-An) <androchentw@gmail.com>
2024-09-15 01:40:26 +0800

    fix ui: back-to-top due to no fadeOut and fadeIn in jquery.slim.min.js
    https://code.jquery.com/jquery-3.7.0.slim.min.js

--- static/js/main.js
+++ static/js/main.js
@@ -103,16 +103,6 @@ var main = {
 
     // feat ui: back to top
     const $backToTop = $('#back-to-top');
-    if ($toc.length) {
-      $(window).scroll(function() {
-        if ($(window).scrollTop() > 100) {
-          $backToTop.fadeIn(1000);
-        } else {
-          $backToTop.fadeOut(1000);
-        }
-      });
-    }
-
     $backToTop.click(function() {
       $('body,html').animate({scrollTop: 0});
     });
